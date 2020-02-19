8dd891d5f6c43fa80e4b8916b199b43489fe86cf
Jimbotsai <42758352+Jimbotsai@users.noreply.github.com>
2020-02-18 18:58:46 -0800

    Add files via upload

--- src/insight.py
+++ src/insight.py
@@ -2,10 +2,13 @@ import csv
 import time
 from datetime import datetime
 import collections
+import os
 
 # read CSV file and keep the information to dt_list
 dt_list = []
-with open('Border_Crossing_Entry_Data_large.csv', mode = 'r') as file:
+
+
+with open(os.path.join(os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir)), 'input/Border_Crossing_Entry_Data_large.csv'), mode = 'r') as file:
 	reader = csv.reader(file, delimiter = ',')
 	next(reader, None)
 	for row in reader:
@@ -35,8 +38,6 @@ def sorting_time(elem):
 	return float(elem[-1])
 dt_list.sort(key=sorting_time)
 
-#print('')
-#print(dt_list)
 
 # add up the values for same [border, measure, timevalue]
 # add the 'cnt' variable for each (border, measure) for the average calculation
@@ -72,18 +73,14 @@ for row in dt_list:
 	ordered_BMT_dic[cur_BMT][0] += int(row[4])
 	ordered_BMT_dic[cur_BMT][1] = BM_dic[cur_BM][0]
 
-
-#print('')
-#print(BM_dic.items())	
-#print('')
-#print('od BMT_dic = ', ordered_BMT_dic)
-
 # write report.csv 
-file = open('report.csv', 'w')
+
+#!! writing elsewhere
+file = open(os.path.join(os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir)), 'output/report.csv'), 'w')
 writer = csv.writer(file)
 writer.writerow(['Border', 'Date', 'Measure', 'Value', 'Average'])
 ordered_BMT_dic = collections.OrderedDict(sorted(ordered_BMT_dic.items(), reverse = True))
-#print('od BMT_dic = ', ordered_BMT_dic)
+
 for key, value in ordered_BMT_dic.items():
 	try:
 		writer.writerow([key[1], datetime.fromtimestamp(float(key[0])).strftime('%m/%d/%Y %H:%M:%S %p'), key[2], value[0], int(round(value[1]/(value[2]-1)))])
